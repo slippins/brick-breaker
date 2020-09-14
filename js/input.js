0019644bcf9caed21a06d6e7c652c4e422609109
Kasper <kasper_klinke@hotmail.com>
2020-09-14 20:13:15 +0200

    indsæt piltast bevægelse

---
+++ js/input.js
@@ -0,0 +1,19 @@
+export default class inputHandler{
+
+    constructor(paddle) {
+        document.addEventListener("keydown", (event) => {
+            switch (event.keyCode) {
+                case 37:
+                    paddle.moveLeft();
+                    break;
+
+                case 39:
+                    paddle.moveRight();
+                    break;
+            }
+
+        }
+        );
+    }
+
+}
