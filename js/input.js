4db705cfbcafcd79b412a6f4529e9cde478930f2
Kasper <kasper_klinke@hotmail.com>
2020-09-14 20:23:59 +0200

    lavet så paddle stopper uden input

--- js/input.js
+++ js/input.js
@@ -12,8 +12,20 @@ export default class inputHandler{
                     break;
             }
 
-        }
-        );
+        });
+
+        document.addEventListener("keyup", (event) => {
+            switch (event.keyCode) {
+                case 37:
+                    if (paddle.speed < 0) paddle.stop();
+                    break;
+
+                case 39:
+                    if (paddle.speed > 0) paddle.stop();
+                    break;
+            }
+
+        });
     }
 
 }
