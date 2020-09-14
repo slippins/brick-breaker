0a25670718dc1fa80cf2c2cbbc35634bd6d7128c
Kasper <kasper_klinke@hotmail.com>
2020-09-14 21:00:02 +0200

    forbedret kode til brug af mange objekter

--- js/game.js
+++ js/game.js
@@ -15,17 +15,17 @@ export default class Game{
         this.paddle = new Paddle(this);
         this.ball = new Ball(this);
 
+        this.gameObjects = [this.ball, this.paddle]
+
         new InputHandler(this.paddle);
     }
 
     update(deltaTime) {
-        this.paddle.update(deltaTime);
-        this.ball.update(deltaTime);
+        this.gameObjects.forEach((object) => object.update(deltaTime));
     }
 
     draw(ctx) {
-        this.paddle.draw(ctx);
-        this.ball.draw(ctx);
+        this.gameObjects.forEach((object) => object.draw(ctx));
     }
 
 }
