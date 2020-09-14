c27bc1d1925dbcc88799205fc45ac2d32d31d7fc
Kasper <kasper_klinke@hotmail.com>
2020-09-14 19:56:54 +0200

    defineret tid

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
+import Paddle from "js/paddle";
+
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
 
-ctx.clearRect(0, 0, 800, 600,);
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
 
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+
+paddle.draw(ctx);
+
+let lastTime = 0;
+
+function gameLoop(timeStamp) {
+    let deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+
+    ctx.clearRect(0, 0, 800, 600,);
+    paddle.update(deltaTime);
+    paddle.draw(ctx)
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
