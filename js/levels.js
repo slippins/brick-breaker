a7e0bf3eeee417231c7659fdcb8a231618dcc42c
Kasper <kasper_klinke@hotmail.com>
2020-09-14 21:32:32 +0200

    lavet levels

---
+++ js/levels.js
@@ -0,0 +1,27 @@
+import Brick from "./brick";
+
+export function buildLevel(){
+    let bricks = [];
+
+    level.forEach((row, rowIndex) => {
+        row.forEach((brick, brickIndex) => {
+            if (brick === 1) {
+                let position = {
+                    x: 80 * brickIndex,
+                    y: 20 + 24 * rowIndex
+                };
+                bricks.push(new Brick(game, position));
+            }
+        });
+    });
+
+    return bricks;
+
+}
+
+export const level1 = [
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
